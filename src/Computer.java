4ea1d3daf42a263b06c912ddc0362622e8bf9adc
dineshjarvis <dinesh20sai96@gmail.com>
2021-07-21 17:04:48 +0530

    jarvis project

---
+++ src/Computer.java
@@ -0,0 +1,6 @@
+
+public class Computer {
+    public void computermodel() {
+		System.out.println("computer model is:LENOVO");
+	}
+}
---
+++ src/Desktop.java
@@ -0,0 +1,11 @@
+
+public class Desktop extends Computer {
+private static void desktopsize() {
+	System.out.println("size is: 3.2 inch");
+}
+public static void main(String[] args) {
+	Desktop c= new Desktop();
+	c.desktopsize();
+	c.computermodel();
+}
+}
